8f25292621a3d94a0b0f14e229ba809cbda110b5
marcus featherston <marcusovfeatherston@gmail.com>
2022-12-21 13:06:46 -0700

    commit tree 6fbe4e0f1215d9eef0d5b460eb6f4b5ad1dbf850

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(catName) {
+  cats.push(catName);
+}
+
+function destructivelyPrependCat(catName) {
+  cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(newCat) {
+  const updatedCatList = [...cats];
+  updatedCatList.push(newCat);
+  return (updatedCatList);
+}
+
+function prependCat(newCat) {
+  const updatedCatList = [...cats];
+  updatedCatList.unshift(newCat);
+  return (updatedCatList)
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  // return cats.slice(1);
+  const updatedCatList = [...cats];
+  updatedCatList.shift()
+  return updatedCatList;
+}
