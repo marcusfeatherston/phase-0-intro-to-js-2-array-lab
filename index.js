918f96912fae14d3e22cadd9c9f0dc2f06cbc602
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-31 16:42:32 -0700

    code for array methods

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+  return cats.push(name);
+};
+
+const destructivelyPrependCat = (name) => {
+  return cats.unshift(name);
+};
+
+const destructivelyRemoveLastCat = (name) => {
+  return cats.pop(name);
+};
+
+const destructivelyRemoveFirstCat = (name) => {
+  return cats.shift(name);
+};
+
+const appendCat = (name) => {
+  const newCats = [...cats, name];
+  return newCats;
+};
+
+const prependCat = (name) => {
+  const newCats = [name, ...cats];
+  return newCats;
+};
+
+const removeLastCat = (name) => {
+  const newCats = [...cats];
+  newCats.pop();
+  return newCats;
+};
+
+const removeFirstCat = (name) => {
+  const newCats = [...cats];
+  newCats.shift();
+  return newCats;
+};
